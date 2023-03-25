5906a559b0be6b599507ff5958535f068b189891
Mohammed Choudhury <Mzchoudhury@outlook.com>
2023-03-25 20:33:03 +0000

    Function defined

--- LeapYear.cpp
+++ LeapYear.cpp
@@ -1,6 +1,20 @@
 #include "LeapYear.h"
 
 bool leapYear(int year) {
-    
+
+    if (year % 4 == 0)
+    {
+        if (year % 100 == 0) 
+        {
+            if (year % 400 == 0)
+            {
+                cout << "Leap Year = True!" << endl;
+            }
+            else cout << "Not leap year!" << endl;
+        }
+        else cout << "Leap Year = True!" << endl;
+    }
+    else cout << "Not leap year!" << endl;
+
     return 0.0;
 }
