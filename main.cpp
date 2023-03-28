dc4c8388c66ad2d291200ea55432fab6a2129fba
Mohammed Zahed Choudhury <mzchoudhury@outlook.com>
2023-03-28 11:58:32 +0100

    Menu created

--- main.cpp
+++ main.cpp
@@ -5,7 +5,36 @@ using namespace std;
 
 int main() {
 
-    cout << leapYear(2001) << endl; 
+    int choice = 0;
+    int year;
+    string name;
+    bool quit = false;
+    bool isLeap;
+
+    do
+    {
+        cout << "Select an option:" << endl;
+        cout << "1) Leap Year Calculator" << endl;
+        cout << "2) Exit" << endl;
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter your name:" << endl;
+            cin >> name;
+            cout << "Enter the year that you would like to check:" << endl;
+            cin >> year;
+            isLeap = leapYear(year);
+            cout << boolalpha << isLeap << endl;
+            cout << "----------------------------" << endl;
+            break;
+
+        case 2:
+            quit = true;
+        }
+
+    } while (!quit);
 
     return 0;
 }
